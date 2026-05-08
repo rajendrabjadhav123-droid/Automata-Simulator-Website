b34d6721851bab47839b03f13e1b26a140f32874
rajendrabjadhav123-droid <rajendrabjadhav123@gmail.com>
2026-05-08 11:35:30 +0530

    Update simulator.js

--- simulator.js
+++ simulator.js
@@ -63,30 +63,30 @@ class PushdownAutomata {
 
         const stateTransitions = this.transitions[this.currentState];
         if (stateTransitions) {
-            // Check for transition with input char
-            let options = stateTransitions[char] || [];
-            
-            // For determinism in this simple simulation, we just pick the first valid transition
-            for (let trans of options) {
-                if (trans.pop === topStack || trans.pop === '') {
-                    // Valid transition
-                    if (trans.pop !== '') {
-                        this.stack.pop();
-                    }
-                    if (trans.push && trans.push.length > 0) {
-                        // Push in reverse order so the first string char is on top
-                        for (let i = trans.push.length - 1; i >= 0; i--) {
-                             if(trans.push[i] !== '') this.stack.push(trans.push[i]);
+            if (char !== '') {
+                // Check for transition with input char
+                let options = stateTransitions[char] || [];
+                
+                // For determinism in this simple simulation, we just pick the first valid transition
+                for (let trans of options) {
+                    if (trans.pop === topStack || trans.pop === '') {
+                        // Valid transition
+                        if (trans.pop !== '') {
+                            this.stack.pop();
                         }
+                        if (trans.push && trans.push.length > 0) {
+                            // Push in reverse order so the first string char is on top
+                            for (let i = trans.push.length - 1; i >= 0; i--) {
+                                 if(trans.push[i] !== '') this.stack.push(trans.push[i]);
+                            }
+                        }
+                        this.currentState = trans.nextState;
+                        return { success: true, nextState: this.currentState, stack: [...this.stack], transitionUsed: trans };
                     }
-                    this.currentState = trans.nextState;
-                    return { success: true, nextState: this.currentState, stack: [...this.stack], transitionUsed: trans };
                 }
-            }
-
-             // Try epsilon transition if regular input char transition failed
-             if (char !== '') {
-                 options = stateTransitions[''] || [];
+            } else {
+                 // Try epsilon transition ONLY when no input symbol is present on tape
+                 let options = stateTransitions[''] || [];
                  for (let trans of options) {
                     if (trans.pop === topStack || trans.pop === '') {
                         if (trans.pop !== '') this.stack.pop();
@@ -96,12 +96,10 @@ class PushdownAutomata {
                             }
                         }
                         this.currentState = trans.nextState;
-                        // Since it was an epsilon transition, the input char was not consumed.
-                        // For simplicity in this step-by-step UI, we will return a flag.
                         return { success: true, nextState: this.currentState, stack: [...this.stack], transitionUsed: trans, epsilon: true };
                     }
                  }
-             }
+            }
         }
         
         return { error: `No transition from ${this.currentState} with input '${char}' and stack top '${topStack}'` };
